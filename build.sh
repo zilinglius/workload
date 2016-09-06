2d9147892c1f82f96286e71f42028689c9621ca2
Rui Ren <magstiff@qq.com>
2016-09-06 23:16:06 +0800

    move code to src

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-g++ general.cpp --std=c++11 -o bin/wsg
-g++ cpu.cpp --std=c++11 -o bin/wsc
+g++ src/general.cpp --std=c++11 -o bin/wsg
+g++ src/cpu.cpp --std=c++11 -o bin/wsc
 git add .
 git commit -m "build"
 git push
